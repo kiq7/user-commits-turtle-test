644ffa87d3f88c4fb41e6a8ce569f9a055350890
Kaique Vieira <kaique.vieira@escale.com.br>
2020-10-12 12:30:11 -0300

    test: unit test for getCommitsByUserAndDate

--- src/infrastructure/repositories/github-repository-info.spec.ts
+++ src/infrastructure/repositories/github-repository-info.spec.ts
@@ -4,40 +4,79 @@ import { RepositoryNotFoundError } from '../../application/errors/repository-not
 describe('GitHub Repository Info', () => {
   const fakeDateUtils: any = {
     formatDateToIso: jest.fn().mockReturnValue('fake-date'),
-    formatDateToLocale: jest.fn().mockReturnValue('fake-date')
+    formatDateString: jest.fn().mockReturnValue('fake-date')
   }
 
-  it('should calls `listCommits` with correct params', async () => {
-    const fakeClient: any = {
-      repos: {
-        listCommits: jest.fn()
+  describe('getCommits', () => {
+    it('should calls `listCommits` with correct params', async () => {
+      const fakeClient: any = {
+        repos: {
+          listCommits: jest.fn()
+        }
       }
-    }
 
-    const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
-    await repository.getCommits('react', 'facebook', '2020-10-05', '2020-10-10')
+      const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
+      await repository.getCommits('react', 'facebook', '2020-10-05', '2020-10-10')
 
-    expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(1, { date: '2020-10-05' })
-    expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(2, { date: '2020-10-10', formatEndDayTime: true })
-    expect(fakeClient.repos.listCommits).toHaveBeenCalledWith({
-      owner: 'react',
-      repo: 'facebook',
-      since: 'fake-date',
-      until: 'fake-date'
+      expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(1, { date: '2020-10-05' })
+      expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(2, { date: '2020-10-10', formatEndDayTime: true })
+      expect(fakeClient.repos.listCommits).toHaveBeenCalledWith({
+        owner: 'react',
+        repo: 'facebook',
+        since: 'fake-date',
+        until: 'fake-date'
+      })
+    })
+
+    it('should throws RepositoryNotFoundError when gh api returns 404', async () => {
+      const fakeNotFoundError = new Error('Fake not found error') as any
+      fakeNotFoundError.status = 404
+
+      const fakeClient: any = {
+        repos: {
+          listCommits: jest.fn().mockRejectedValue(fakeNotFoundError)
+        }
+      }
+
+      const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
+      await expect(repository.getCommits('react', 'facebook', '2020-10-05', '2020-10-10')).rejects.toThrowError(RepositoryNotFoundError)
     })
   })
 
-  it('should throws RepositoryNotFoundError when gh api returns 404', async () => {
-    const fakeNotFoundError = new Error('Fake not found error') as any
-    fakeNotFoundError.status = 404
-
-    const fakeClient: any = {
-      repos: {
-        listCommits: jest.fn().mockRejectedValue(fakeNotFoundError)
+  describe('getCommitsByUserAndDate', () => {
+    it('should calls `listCommits` with correct params', async () => {
+      const fakeClient: any = {
+        repos: {
+          listCommits: jest.fn()
+        }
       }
-    }
 
-    const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
-    await expect(repository.getCommits('react', 'facebook', '2020-10-05', '2020-10-10')).rejects.toThrowError(RepositoryNotFoundError)
+      const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
+      await repository.getCommitsByUserAndDate('react', 'facebook', 'fake-author', '2020-10-10')
+
+      expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(1, { date: '2020-10-10' })
+      expect(fakeDateUtils.formatDateToIso).toHaveBeenNthCalledWith(2, { date: '2020-10-10', formatEndDayTime: true })
+      expect(fakeClient.repos.listCommits).toHaveBeenCalledWith({
+        owner: 'react',
+        repo: 'facebook',
+        author: 'fake-author',
+        since: 'fake-date',
+        until: 'fake-date'
+      })
+    })
+
+    it('should throws RepositoryNotFoundError when gh api returns 404', async () => {
+      const fakeNotFoundError = new Error('Fake not found error') as any
+      fakeNotFoundError.status = 404
+
+      const fakeClient: any = {
+        repos: {
+          listCommits: jest.fn().mockRejectedValue(fakeNotFoundError)
+        }
+      }
+
+      const repository = new GitHubRepositoryInfo(fakeClient, fakeDateUtils)
+      await expect(repository.getCommitsByUserAndDate('react', 'facebook', 'fake-author', '2020-10-10')).rejects.toThrowError(RepositoryNotFoundError)
+    })
   })
 })
